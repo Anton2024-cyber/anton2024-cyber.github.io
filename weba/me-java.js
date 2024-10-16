5e5653e0c804d40787643b076c5941835552f1fe
Anton2024-cyber <anton26110526@mail.ru>
2024-10-16 16:58:42 +0300

    me-java.js

--- weba/me-java.js
+++ weba/me-java.js
@@ -17,3 +17,37 @@ function onClick(event) {
     let b = document.getElementById("button1");
     b.addEventListener("click", onClick);
   });
+window.addEventListener('DOMContentLoaded', function (event) {
+    let s = document.getElementsByName("select1");
+    s[0].addEventListener("change", function(event) {
+      let select = event.target;
+      let radios = document.getElementById("myradios");
+      let checkboxes = document.getElementById("mycheckboxs");
+      console.log(select.value);
+      if (select.value == "tovar2") {
+        radios.style.display = "none";
+      }
+      else {
+        radios.style.display = "block";
+      }
+      if (select.value == "tovar3") {
+        checkboxes.style.display = "none";
+      }
+      else {
+        checkboxes.style.display = "block";
+      }
+      if (select.value == "tovar4") {
+        checkboxes.style.display = "none";
+        radios.style.display = "none";
+      }
+    });
+    
+    let r = document.querySelectorAll(".myradios input[type=radio]");
+    r.forEach(function(radio) {
+      radio.addEventListener("change", function(event) {
+        let r = event.target;
+        console.log(r.value);
+      });    
+    });
+    
+  });
