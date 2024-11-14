07901f57181cf298476b8e09354c945991d19454
Anton2024-cyber <anton26110526@mail.ru>
2024-11-14 20:52:01 +0300

    Add files via upload

---
+++ popa/jajaja.js
@@ -0,0 +1,46 @@
+const dialog = document.getElementById('myDialog')
+const dialogOpener = document.querySelector('.openDialogBtn')
+const dialogCloser = dialog.querySelector('.closeDialogBtn')
+
+function closeOnBackDropClick({ currentTarget, target }) {
+  const dialog = currentTarget
+  const isClickedOnBackDrop = target === dialog
+  if (isClickedOnBackDrop) {
+    close()
+  }
+}
+
+function openModalAndLockScroll() {
+  dialog.showModal()
+  document.body.classList.add('scroll-lock')
+}
+
+function returnScroll() {
+  document.body.classList.remove('scroll-lock')
+}
+
+function close() {
+  dialog.close()
+  returnScroll()
+}
+
+dialog.addEventListener('click', closeOnBackDropClick)
+dialog.addEventListener('cancel', (event) => {
+  returnScroll()
+});
+dialogOpener.addEventListener('click', openModalAndLockScroll)
+dialogCloser.addEventListener('click', (event) => {
+  event.stopPropagation()
+  close()
+})
+
+function onClick(event) {
+  event.preventDefault();
+  alert("form send");
+}
+window.addEventListener('DOMContentLoaded', function (event) {
+  console.log("DOM fully loaded and parsed");
+  let b = document.getElementById("button1");
+  b.addEventListener("click", onClick);
+});
+
