d9fee406b522dd19c361ae225484d5202f6ef704
Anton2024-cyber <anton26110526@mail.ru>
2025-04-16 16:51:58 +0300

    Add files via upload

---
+++ success.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Успех</title>
+</head>
+<body>
+    <h1>Форма успешно отправлена!</h1>
+    <p>Спасибо за заполнение формы.</p>
+    <a href="form.php">Вернуться к форме</a>
+</body>
+</html>
