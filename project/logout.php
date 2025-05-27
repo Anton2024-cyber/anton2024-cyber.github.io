b2cc5847bd6448928b19ce880ee66c413413e60e
Anton2024-cyber <anton26110526@mail.ru>
2025-05-27 21:14:13 +0300

    Update logout.php

--- project/logout.php
+++ project/logout.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
 session_destroy();
-header('Location: index.html'); // Перенаправление на страницу регистрации
+header('Location: project.html'); // Перенаправление на страницу регистрации
 exit();
 ?>
