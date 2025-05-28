304f374f80fe18c08b38c9e4ef6bd91da7db47f2
Anton2024-cyber <anton26110526@mail.ru>
2025-05-28 13:52:29 +0300

    Update jiji.js

--- jiji.js
+++ jiji.js
@@ -7,6 +7,7 @@ document.getElementById('userForm').addEventListener('submit', function(event) {
     formData.forEach((value, key) => {
         jsonData[key] = value;
     });
+
     fetch('log.php', {
         method: 'POST',
         headers: {
