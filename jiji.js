849fc604e507bb367d959d968506e356984ea5ca
Anton2024-cyber <anton26110526@mail.ru>
2025-05-28 17:18:17 +0300

    Update jiji.js

--- jiji.js
+++ jiji.js
@@ -7,7 +7,7 @@ document.getElementById('userForm').addEventListener('submit', function(event) {
     formData.forEach((value, key) => {
         jsonData[key] = value;
     });
-
+    
     fetch('log.php', {
         method: 'POST',
         headers: {
@@ -23,6 +23,39 @@ document.getElementById('userForm').addEventListener('submit', function(event) {
         } else {
             // Перенаправление на project.html после успешной отправки
             window.location.href = 'project.html';
+        }
+    })
+    .catch(error => {
+        console.error('Ошибка:', error);
+    });
+});
+
+// Обработка формы входа в админку
+document.getElementById('adminLoginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Предотвращаем стандартное поведение формы
+
+    const formData = new FormData(this);
+    const jsonData = {};
+
+    formData.forEach((value, key) => {
+        jsonData[key] = value;
+    });
+
+    fetch('admin_view.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(jsonData)
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Обработка ответа от сервера
+        if (data.error) {
+            document.getElementById('adminResponse').innerText = data.error;
+        } else {
+            // Перенаправление на admin.php после успешного входа
+            window.location.href = 'admin.php';
         }
     })
     .catch(error => {
