312547ee6d512256b70dd751ede02af4098fc5f3
Anton2024-cyber <anton26110526@mail.ru>
2025-05-28 17:03:08 +0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
 session_destroy();
-header('Location: login.php'); // Redirect to admin login page
+header('Location: login.php'); // Перенаправление на страницу входа для администраторов
 exit();
 ?>
